69a0c26e65da7947f282d58a55dd3659439293a3
pvsukalkar <pvsukalkar@iitj.ac.in>
2015-05-01 13:26:05 +0530

    Added pause option in stopwatch

--- Stopwatch/stopwatch.sh
+++ Stopwatch/stopwatch.sh
@@ -10,18 +10,30 @@ read option
 case ${option} in
    1)
    echo "Starting stopwatch..."
+   echo "Enter P to pause and Ctrl+c to stop "
    BEGIN="$(date +%s)"
    while true; do
-   	NOW=$(date +%s)
-   	DIFF=$(($NOW - $BEGIN))
-   	MINS=$(($DIFF / 60))
- 	SECS=$(($DIFF % 60))
-   	HOURS=$(($DIFF / 3600))
-   	DAYS=$(($DIFF / 86400))
- 
-   	# \r  is a "carriage return" - returns cursor to start of line
-   	printf "\r%3d ${LIGHT_GREEN}Days${NC},${LIGHT_BLUE}%02d:%02d:%02d${NC}" $DAYS $HOURS $MINS $SECS
-   	sleep 0.25
+        NOW=$(date +%s)
+        DIFF=$(($NOW - $BEGIN))
+        MINS=$(($DIFF / 60))
+        SECS=$(($DIFF % 60))
+        HOURS=$(($DIFF / 3600))
+        DAYS=$(($DIFF / 86400))
+
+        # \r  is a "carriage return" - returns cursor to start of line
+        printf "\r%3d ${LIGHT_GREEN}Days${NC},${LIGHT_BLUE}%02d:%02d:%02d${NC}" $DAYS $HOURS $MINS $SECS
+        read -t 0.25 pause
+        if [[ "$pause" == "P" ]] ; then
+            CURRENT_TIME=$(date +%s)
+            PAUSE_TIME=$((CURRENT_TIME - BEGIN))
+            read input
+            while [[ $input != "P"  ]] ; do
+               echo "Enter P to restart"
+               read input
+            done
+            RESTART_TIME=$(date +%s)
+            BEGIN=$((RESTART_TIME - PAUSE_TIME ))
+        fi
    done ;;
    2) 	echo "Starting Lap timer..." 
       	echo "Enter L to start new lap"
