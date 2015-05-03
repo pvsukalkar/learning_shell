6b88c76efd8eb246a9febf5fd9258244d3ba6df3
pvsukalkar <pvsukalkar@iitj.ac.in>
2015-05-03 12:36:26 +0530

    rain of any string that user inputs

--- matrix/matrixrain.sh
+++ matrix/matrixrain.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "Enter String"
+read name
+length=${#name}
 #clearing terminal
 echo -e "\033[2J\033[?251"
 
@@ -15,10 +18,17 @@ while true ; do
   j=$[RANDOM%C]       #j deals with columns
   d=$[RANDOM%R]       #d deals with rows or lines
   for i in `eval echo -e {1..$R}`; do   #'eval' is used to execute command by joining parts in shell
-    c=`printf '\\\\0%o' $[RANDOM%57+33]`   #storing octal value greater than \033
+    #c=`printf '\\\\0%o' $[RANDOM%57+33]`   #storing octal value greater than \033
     #\033[23  --> means print something on 23rd line from top. \033[24 --> means print something from 24th line from top 
     #\033[32m --> for green color
-    echo -e "\033[$[i-1];${j}H\033[32m$c\033[$i;${j}H\033[37m"$c   #Don't know exactly why 'H' is added 
+    #if [[ i%2 -eq 0 ]] ; then 
+    #   c=0
+    #else 
+    #   c=1
+    #fi
+    index=$[i%$length]
+    c=${name:index:1} 
+    echo -e "\033[$[i-1];${j}f\033[32m$c\033[$i;${j}f\033[37m"$c   #${j}H prints character horizontally on j th column 
     sleep 0.1
     if [ $i -ge $d ] ; then
     	echo -e "\033[$[i-d];${j}H "    
@@ -26,7 +36,7 @@ while true ; do
   done
   
   for i in `eval echo -e {$[i-d]..$R}`; do
-	echo -e "\033[$i;${j}f "
+	echo -e "\033[$i;${j}H "
         sleep 0.1
   done
 )&     #this process is running in background
